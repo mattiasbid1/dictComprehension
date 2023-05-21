51b0b830d168846d0360491ab122a1c40e42182f
Mattias Björk <mattias.bjork.id1@gmail.com>
2023-05-21 15:44:08 +0200

    final test

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-# list comps
+import random
+
+listerino = ["Snake", "Worm", "Wurm", "Dragon"]
+
+new_dict = {item:random.randint(1,100) for item in listerino}
+
+print(new_dict)
+
+
+supernew_dict = {n:m for (n,m) in new_dict.items() if m > 70}
+
+print(supernew_dict)
+
+
+
 
